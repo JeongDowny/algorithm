c0d6b374c3ca618bfedff8f1122bdf543a472ae3
JeongDowny <daun5535@naver.com>
2024-09-15 16:53:05 +0900

    Solve problems 1780 and 4780

--- 4779_cantorSet_S3.cpp
+++ 4779_cantorSet_S3.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
-void cantor(int n, vector<char> str) {
-    if(find(str.begin(), str.end(), '-')!=str.end()) {
-        vector<char> tempStr1(str.size()/3, '-');
-        vector<char> tempStr2(str.size()/3, ' ');
-        vector<char> tempStr3 = tempStr1;
+void cantor(vector<char> str)
+{
+    vector<char> tempStr1(str.size() / 3), tempStr2(str.size() / 3), tempStr3(str.size() / 3);
+    if (find(str.begin(), str.end(), '-') != str.end())
+    {
+        fill(tempStr1.begin(), tempStr1.end(), '-');
+        fill(tempStr2.begin(), tempStr2.end(), ' ');
+        tempStr3 = tempStr1;
     }
-    else {
-        vector<char> tempStr1(str.size()/3, ' ');
-        vector<char> tempStr2 = tempStr1;
-        vector<char> tempStr3 = tempStr1;
+    else
+    {
+        fill(tempStr1.begin(), tempStr1.end(), ' ');
+        tempStr2 = tempStr1;
+        tempStr3 = tempStr1;
     }
 
-    if(tempStr1.size() != 1) {
-        cantor(n-1, tempStr1);
-        cantor(n-1, tempStr2);
-        cantor(n-1, tempStr3);
+    if (tempStr1.size() != 1)
+    {
+        cantor(tempStr1);
+        cantor(tempStr2);
+        cantor(tempStr3);
     }
-    else {
-        cout<<tempStr1[0]<<tempStr2[0]<<tempStr3[0];
+    else
+    {
+        cout << tempStr1[0] << tempStr2[0] << tempStr3[0];
     }
 }
 
@@ -30,13 +38,14 @@ int main()
     cin.tie(NULL);
 
     int n;
-    cin>>n;
-
-    vector<char> str;
-    for(int i=0; i<pow(3, n); i++) {
-        char c = '-';
-        str.push_back(c);
+    while (cin >> n)
+    {
+        if(n==0) {
+            cout<<'-'<<'\n';
+            continue;
+        }
+        vector<char> str(pow(3, n), '-');
+        cantor(str);
+        cout<<'\n';
     }
-
-    cantor(n, str);
 }
